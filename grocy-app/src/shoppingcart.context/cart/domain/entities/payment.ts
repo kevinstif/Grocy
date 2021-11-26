60e62ef36b0fa61520e29d367126bd69f59bdcbf
William <axeso_william@hotmail.com>
2021-11-26 17:01:51 -0500

    payment-entity

---
+++ grocy-app/src/shoppingcart.context/cart/domain/entities/payment.ts
@@ -0,0 +1,36 @@
+import { Money } from "../../../../common/domain/value-objects/money.value";
+import { Entity } from "../../../../common/domain/entities/entity";
+import { DateTime } from "../../../../common/domain/value-objects/date-time.value";
+
+export class Payment extends Entity{
+
+  private customerId:number;
+  private cartId:number;
+  private price:Money;
+  private date:DateTime;
+
+  public constructor(id:number,customerId:number,cartId:number,price:Money,date:DateTime) {
+    super(id);
+    this.customerId=customerId;
+    this.cartId=cartId;
+    this.price=price;
+  }
+
+  public paidOut(){
+
+  }
+
+  public getCustomerId():number{
+    return this.customerId;
+  }
+  public getCartId():number{
+    return this.cartId;
+  }
+  public getPrice():Money{
+    return this.price;
+  }
+
+  public getDate():DateTime{
+    return this.date;
+  }
+}
